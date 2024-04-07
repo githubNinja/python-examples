d01588d89754dded4cd64611638350822f7a15bc
githubNinja <githubkran@gmail.com>
2024-04-07 15:00:21 -0500

    fix: Amend

--- examples/chatgpt/gpt-3.5.turbo.py
+++ examples/chatgpt/gpt-3.5.turbo.py
@@ -1,12 +1,20 @@
 import os
 import openai
+<<<<<<< HEAD
 OPENAI_API_KEY='#################'
+=======
+OPENAI_API_KEY='#####'
+>>>>>>> b322544 (fix:Remove personal api key)
 
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
 print('openapi key:::', openai.api_key)
 
 #Instructions - Set the API Key in venv terminal as
+<<<<<<< HEAD
+=======
+#OPENAI_API_KEY="#"
+>>>>>>> b322544 (fix:Remove personal api key)
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
